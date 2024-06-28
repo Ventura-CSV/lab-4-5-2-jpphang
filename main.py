4f0b4250b58c819b966479a8c81e2bcbaf1c8224
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-28 10:07:42 -0700

    52

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ import random
 def main():
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+
     
     while total <= 100:
         ran_num = random.randint(1,100)
@@ -17,9 +13,7 @@ def main():
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
+
     return numbers, total
 
 
