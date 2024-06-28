ff1cefb36da938293a5fa416d592064ab6d3d86e
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-28 10:35:15 -0700

    52

--- main_test.py
+++ main_test.py
@@ -1,27 +1,22 @@
-import main
-import io
-import sys
-import re
+import random
 
+def main():
+    total = 0
+    numbers = []
 
-def test_main_1():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = 'A\nB\n30'
-    # sys.stdin = io.StringIO(datastr)
+    while total <= 100:
+        ran_num = random.randint(1, 100)
+        numbers.append(ran_num)
+        total += ran_num
 
-    numbers, total = main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
+    # Adjust total to be the sum of all numbers except the last one
+    total -= numbers[-1]
 
-    gt100 = [v for v in numbers if v > 100]
-    assert gt100 == [], 'Random values should be >0 and <100'
-    assert total == sum(numbers[:-1]), 'Total is not valid'
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    # regex_string = r'[\w,\W]*' + str(tot) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[1])
-    # assert res != None
-    # print(res.group())
+    # Print statements for debugging (can be removed if not needed)
+    print(f'The random values are {numbers}')
+    print(f'The total is {total}')
+
+    return numbers, total
+
+if __name__ == '__main__':
+    main()
