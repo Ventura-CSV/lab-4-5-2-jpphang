b544e0a3cb8096181b742cfe4bb28a7f6b3a927c
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-28 10:50:39 -0700

    52

--- main_test.py
+++ main_test.py
@@ -1,22 +1,27 @@
-import random
+import main
+import io
+import sys
+import re
 
-def main():
-    total = 0
-    numbers = []
 
-    while total <= 100:
-        ran_num = random.randint(1, 100)
-        numbers.append(ran_num)
-        total += ran_num
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = 'A\nB\n30'
+    # sys.stdin = io.StringIO(datastr)
 
-    # Adjust total to be the sum of all numbers except the last one
-    total -= numbers[-1]
+    numbers, total = main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
 
-    # Print statements for debugging (can be removed if not needed)
-    print(f'The random values are {numbers}')
-    print(f'The total is {total}')
-
-    return numbers, total
-
-if __name__ == '__main__':
-    main()
+    gt100 = [v for v in numbers if v > 100]
+    assert gt100 == [], 'Random values should be >0 and <100'
+    assert total == sum(numbers[:-1]), 'Total is not valid'
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # regex_string = r'[\w,\W]*' + str(tot) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[1])
+    # assert res != None
+    # print(res.group())
