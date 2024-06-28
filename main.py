567ef07c2cbf4427b6118f5bce8f9ef59de49956
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-28 10:07:20 -0700

    51

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def main():
     """
     
     while total <= 100:
-        ran_num = random randint(1,100)
+        ran_num = random.randint(1,100)
+        numbers append(ran_num)
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
